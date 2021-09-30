9d626eebf312fdfe64b7e9ec038669502e477469
nelsesepta <nelseseptahenidar@gmail.com>
2021-09-30 20:08:17 +0700

    title

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,26 @@ import Hero from "../components/Hero";
 import Member from "../components/Member";
 import Blog from "../components/Blog";
 import Join from "../components/Join";
+import Head from "next/head";
 import CardAbout from "../components/molecules/CardAbout";
 import FAQ from "../components/FAQ";
 
 export default function Home() {
   return (
-    <Layout>
-      <Hero />
-      <Member />
-      <Blog />
-      <div className="bg-primary px-5 sm:px-10 pt-10 sm:pt-20 pb-36">
-        <CardAbout />
-      </div>
-      <Join />
-      <FAQ />
-    </Layout>
+    <>
+      <Head>
+        <title>Travel-One-Page</title>
+      </Head>
+      <Layout>
+        <Hero />
+        <Member />
+        <Blog />
+        <div className="bg-primary px-5 sm:px-10 pt-10 sm:pt-20 pb-36">
+          <CardAbout />
+        </div>
+        <Join />
+        <FAQ />
+      </Layout>
+    </>
   );
 }
